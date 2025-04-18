a3828b3e2f5ddced82a36d3b32d8616445a7707f
kushalstcet666@gmail.com <kushalstcet666@gmail.com>
2025-04-18 18:33:12 +0530

    2nd commit: user model

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    student_id = models.CharField(max_length=5, unique= True)
+    mood = models.CharField(max_length=100, blank=True, null= True)
+    sreak_days = models.IntegerField(default=0)
